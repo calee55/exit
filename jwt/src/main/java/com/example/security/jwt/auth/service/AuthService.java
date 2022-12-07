5ba851dd178ed571d0803cd4df6151d461435519
blacksmith92 <calee55@naver.com>
2022-12-07 23:52:41 +0900

    Delete jwt directory

--- jwt/src/main/java/com/example/security/jwt/auth/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.example.security.jwt.auth.service;
-
-import com.example.security.jwt.auth.dto.ResponseAuth;
-
-public interface AuthService {
-    ResponseAuth.Token authenticate(String username, String password);
-    ResponseAuth.Token refreshToken(String refreshToken);
-    void invalidateRefreshTokenByUsername(String username);
-}
